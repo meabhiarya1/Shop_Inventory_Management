8541f9bb2db2d546ec65b5d6c0ff4e7eba628fa0
Abhishek kumar <abhi.arya97161@gmail.com>
2024-06-16 18:50:49 +0530

    database connect and .env added

--- BackEnd/src/db/conn.js
+++ BackEnd/src/db/conn.js
@@ -1,14 +1,27 @@
-const moongoose = require("moongoose");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-moongoose
-  .connect("mongodb://localhost:27017/Shop_Inventory", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
+dotenv.config();
+
+const DB = process.env.DATABASE;
+mongoose
+  .connect(DB)
   .then(() => {
-    console.log("connection successful");
+    console.log("Connection Successful");
   })
   .catch((error) => {
     console.log(error);
   });
+
+// moongoose
+//   .connect("mongodb://localhost:27017/Shop_Inventory", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//   })
+//   .then(() => {
+//     console.log("connection successful");
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
